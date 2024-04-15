7204ec8737d1562f3ef60fec55f2e7baf7b4afb9
Dery Jon <yusupoviskandar1005@gmail.com>
2024-04-15 16:33:37 +0500

    feat: add new order page (2024-04-15, 16:33)

--- pages/order/new-order.vue
+++ pages/order/new-order.vue
@@ -1,8 +1,13 @@
 <script lang="ts" setup>
-import {ref} from 'vue'
+import { ref } from 'vue'
 import InputSearch from '../../components/layout/InputSearch.vue'
 
 const numberProduct = ref(1)
+const selectedButton = ref('UZS');
+
+const selectButton = (button: string) => {
+  selectedButton.value = button;
+};
 </script>
 <template>
     <section class="new-order flex ">
@@ -18,12 +23,12 @@ const numberProduct = ref(1)
                         <p>Возврат</p>
                     </button>
                 </div>
-                <div class="body mt-10">
-                    <div class="top flex items-center gap-10">
+                <div class="body mt-5">
+                    <div class="top flex items-center gap-6">
 
                         <h3 class="text-xl lg:text-3xl font-semibold
                         ">Корзина</h3>
-                        <div class="etc bg-[#404040] p-2 rounded-lg">
+                        <div class="etc bg-[#404040] p-2 rounded-xl">
                             1
                         </div>
                     </div>
@@ -35,19 +40,21 @@ const numberProduct = ref(1)
                             <Icon name="ic:baseline-plus" />
                         </button>
                     </div>
-                    <!-- <div class="cart h-[300px] rounded-2xl mt-10 ">
+                    <!-- <div class="cart h-[300px] rounded-2xl mt-5 ">
                         <p class="text-center text-lg mt-[130px]">Корзинка пока x`что пуста</p>
                     </div> -->
-                    <div class="cards mt-10">
+                    <div class="cards mt-5">
                         <div class="card bg-[#404040] p-4 rounded-2xl flex items-center justify-between">
                             <div class="left flex items-center gap-6">
 
                                 <div class="etc flex items-center gap-2">
-                                    <input v-model="numberProduct" type="number" class="w-[50px] h-[30px] border rounded-lg"> шт
+                                    <input v-model="numberProduct" type="number"
+                                        class="w-[50px] h-[30px] border rounded-lg px-[10px]"> шт
                                 </div>
                                 <div class="img-product">
-                                    <img  src="../../assets/icons/placeholder_img.svg" alt="Photo" class="photo-cell mx-auto">
-    
+                                    <img src="../../assets/icons/placeholder_img.svg" alt="Photo"
+                                        class="photo-cell mx-auto">
+
                                 </div>
                                 <div class="name-product">
                                     <p>Паяльник</p>
@@ -58,10 +65,10 @@ const numberProduct = ref(1)
 
                                 <div class="price-product flex items-center gap-2 text-[#1F78FF] cursor-pointer">
                                     <p class="text-xl font-semibold">100000 UZS</p>
-                                    <Icon name="ic:baseline-edit" size="20"/>
+                                    <Icon name="ic:baseline-edit" size="20" />
                                 </div>
                                 <button class="delete hover:text-red-500 p-2">
-                                    <Icon name="fa:trash" size="20"/>
+                                    <Icon name="fa:trash" size="20" />
                                 </button>
                             </div>
                         </div>
@@ -71,10 +78,55 @@ const numberProduct = ref(1)
 
             </div>
         </div>
-        <div class="right w-[300px] p-5">
+        <div class="right w-[300px] p-5 h-[630px]">
             <div class="clients">
+                <div class="top flex justify-between font-semibold text-lg ">
 
-                <p class="">Клиент</p>
+                    <p class="">Клиент</p>
+                    <button class="create text-[#1F78FF]">
+                        Создать
+                    </button>
+                </div>
+                <div class="bg-[#3b3b3b] mt-4 px-2 py-4 rounded-2xl flex gap-3">
+                    <Icon name="heroicons:user" class="text-[#1F78FF]" size="24" />
+                    <input type="text bg-tr" placeholder="Имя и номер клиента">
+                </div>
+
+
+            </div>
+            <div class="sales my-10">
+
+
+                <p class="">Скидка</p>
+                <div class="flex gap-2 mt-4">
+                    <div class="bg-[#3b3b3b] w-[130px] px-2 py-4 rounded-2xl">
+                        <input type="text" placeholder="Введите скидку" class="w-[120px]" />
+                    </div>
+                    <div class="flex items-center bg-[#404040] rounded-2xl w-[130px]">
+                        <button class="p-1 py-4 w-1/2 rounded-2xl" :class="{ 'border': selectedButton === 'UZS' }"
+                            @click="selectButton('UZS')">
+                            UZS
+                        </button>
+                        <button class="p-1 py-4 w-1/2 rounded-2xl" :class="{ 'border': selectedButton === '%' }"
+                            @click="selectButton('%')">
+                            %
+                        </button>
+                    </div>
+                </div>
+
+
+            </div>
+            <div class="bg-[#404040] rounded-2xl p-5 shadow-2xl mt-24">
+                <div class="obs flex items center justify-between text-md p-2">
+                    <p>Подитог</p>
+                    <p>1 UZS</p>
+
+                </div>
+                <div class="discount flex items center justify-between text-md p-2">
+                    <p>Скидка</p>
+                    <p>100 UZS</p>
+                </div>
+                <div class="btn bg-[#1F78FF] rounded-2xl p-5 text-center">Оплатить</div>
             </div>
         </div>
         <!-- будем делать через модалку при нахождение товаров должен открыться модалка и выйти товары при нажатие на них должен добавляться в [] чтобы я мог отправить запрос через [] -->
@@ -84,6 +136,8 @@ const numberProduct = ref(1)
 .cart {
     border: 1px #ffffff dashed;
 }
-input {outline:none;}
 
+input {
+    outline: none;
+}
 </style>
