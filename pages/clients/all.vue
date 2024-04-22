86bcb3c62bf7cb9ee86bff9ba0a42371b7a04aad
Dery Jon <yusupoviskandar1005@gmail.com>
2024-04-22 16:38:57 +0500

    change: edit api (2024-04-22, 16:38)

---
+++ pages/clients/all.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+// import CreateBtn from './components/CreateBtn.vue';
+import InputSearch from '../../components/layout/InputSearch.vue'
+import ClientsTable from './components/ClientsTable.vue';
+const open = ref(false);
+const toggleOpen = () => {
+    open.value = !open.value;
+};
+
+
+</script>
+
+<template>
+    <section class="catalog">
+        <div class="top flex items-center justify-between">
+            <h2 class="text-2xl lg:text-4xl font-semibold ">Сотрудники</h2>
+        </div>
+        <div class="input flex justify-between mt-[50px] ">
+
+            <InputSearch class="" />
+
+            <router-link to="/management/create">
+
+                <!-- <CreateBtn /> -->
+            </router-link>
+        </div>
+
+        <ClientsTable />
+    </section>
+</template>
+<style scoped>
+.photo-cell {
+    width: 50px;
+    height: 50px;
+    object-fit: cover;
+}
+</style>
---
+++ pages/clients/create.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+import CreateBtn from './components/CreateBtn.vue';
+import ExitButton from '../../components/layout/ExitButton.vue';
+import { useRouter } from 'vue-router';
+import { base_url } from '~/api';
+import { ref } from 'vue'
+
+const name = ref<string>('');
+const login = ref<string>('');
+const role = ref<string>('');
+const password = ref<string>('');
+const router = useRouter();
+
+const createWorker = async () => {
+    const body = {
+        fio: name.value,
+        login: login.value,
+        role: role.value,
+        password: password.value,
+    };
+    const token = localStorage.getItem('token') || '';
+    await useFetch(`${base_url}/user`, {
+        method: 'POST',
+        headers:{
+    "Authorization": "Bearer " + token,
+},
+        body: JSON.stringify(body),
+    }).then(res => {
+        if (res.status === 201) {
+            router.push({ name: 'ManagementList' })
+        }
+    })
+};
+
+</script>
+<template>
+    <section class="basic">
+        <div class="flex items-center justify-between mt-6">
+            <router-link to="/management/list">
+
+                <ExitButton />
+            </router-link>
+            <h2 class="text-4xl font-semibold ml-5">Новый сотрудник</h2>
+            <CreateBtn class="ml-auto" @click="createWorker" />
+        </div>
+        <div class="basic">
+            <div class="flex flex-wrap gap-[30px] justify-between mt-10">
+                <div class="name w-1/3">
+                    <label for="">Имя</label>
+                    <UiInput placeholder="Имя сотрудника" v-model="name" />
+                </div>
+                <div class="name w-1/3">
+                    <label for="">Логин</label>
+                    <UiInput placeholder="Логин сотрудника" v-model="login" />
+                </div>
+                <div class="role w-1/3 ">
+                    <label for="">Роль</label>
+                    <select placeholder="Роль сотрудника" v-model="role"
+                        class="w-full text-[#f5f5f5] py-4 px-3 rounded-2xl mt-5 border">
+                        <option value="salesman">Продавец</option>
+                        <option value="manager">Менеджер</option>
+                        <option value="director">Директор</option>
+                    </select>
+                </div>
+                <div class="role w-1/3">
+                    <label for="">Пароль</label>
+                    <UiInput placeholder="Пароль сотрудника" v-model="password" />
+                </div>
+
+            </div>
+        </div>
+    </section>
+</template>
