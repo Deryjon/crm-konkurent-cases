e9384ea70db885f4831650156e3f2e7ccd5edb14
Dery Jon <yusupoviskandar1005@gmail.com>
2024-05-14 11:37:02 +0500

    feat: add

--- pages/products/components/Stat.vue
+++ pages/products/components/Stat.vue
@@ -1,19 +1,19 @@
 <script setup lang="ts">
-import {ref} from 'vue'
-import {base_url} from '~/api'
+import { ref } from 'vue'
+import { base_url } from '~/api'
 const statsProduct = ref({})
 const valyutaKurs = ref(0)
- const fetchStats = async () => {
+const fetchStats = async () => {
     const token = localStorage.getItem("token");
-    if (!token) return;   
+    if (!token) return;
     const { data } = await useFetch(
-      `${base_url}/product/stats`,
-      {
-        method: "GET",
-        headers: {
-          Authorization: "Bearer " + token,
-        },
-      }
+        `${base_url}/product/stats`,
+        {
+            method: "GET",
+            headers: {
+                Authorization: "Bearer " + token,
+            },
+        }
     );
     statsProduct.value = data.value
 };
@@ -24,51 +24,51 @@ const fetchValyuta = async () => {
     if (status.value === "success") {
         console.log(data.value);
         valyutaKurs.value = data.value[0].Rate
-    } 
+    }
 }
 fetchValyuta()
 onMounted(() => {
-  fetchStats()
+    fetchStats()
 });
 
 </script>
 <template>
     <section class="stats flex flex-wrap justify-between">
 
-        <div class="w-[80px] lg:w-[250px] bg-[#353535] rounded-2xl p-1 lg:p-3" >
+        <div class="w-[80px] lg:w-[250px] bg-[#353535] rounded-2xl p-1 lg:p-3">
             <Icon name="heroicons:cube" class="h-5 w-5 text-muted-foreground" />
             <p class="text-sm">Наименований</p>
-<div class="flex items-center gap-1">
+            <div class="flex items-center gap-1">
 
-    <p>{{ statsProduct.total_products }}</p>
-    <p>шт</p>
-</div>
+                <p>{{ statsProduct?.total_products }}</p>
+                <p>шт</p>
+            </div>
         </div>
-        
-        <div class="w-[80px] lg:w-[250px] bg-[#353535] rounded-2xl p-1 lg:p-3" >
+
+        <div class="w-[80px] lg:w-[250px] bg-[#353535] rounded-2xl p-1 lg:p-3">
             <Icon name="heroicons:cube" class="h-5 w-5 text-muted-foreground" />
             <p class="text-sm">Товарных единиц</p>
             <div class="flex items-center gap-1">
-            <p>{{ statsProduct.total_quantity }}</p>
-            <p>ед.</p>
+                <p>{{ statsProduct?.total_quantity }}</p>
+                <p>ед.</p>
+            </div>
         </div>
-        </div>
-        <div class="w-[80px] lg:w-[250px] bg-[#353535] rounded-2xl p-1 lg:p-3" >
+        <div class="w-[80px] lg:w-[250px] bg-[#353535] rounded-2xl p-1 lg:p-3">
             <Icon name="ph:money" class="h-5 w-5 text-muted-foreground" />
             <p class="text-sm">Сумма по цене продажи</p>
             <div class="flex items-center gap-1">
-            <p>{{ statsProduct.total_stock_value }}</p>
-            <p>USD</p>
+                <p>{{ statsProduct?.total_stock_value }}</p>
+                <p>USD</p>
+            </div>
         </div>
-        </div>
-        <div class="w-[80px] lg:w-[250px] bg-[#353535] rounded-2xl p-1 lg:p-3" >
+        <div class="w-[80px] lg:w-[250px] bg-[#353535] rounded-2xl p-1 lg:p-3">
             <Icon name="ph:money" class="h-5 w-5 text-muted-foreground" />
             <p class="text-sm">Сумма по цене продажи</p>
             <div class="flex items-center gap-1">
-            <p>{{ Math.round(statsProduct.total_stock_value * valyutaKurs) }}</p>
-            <p>UZS</p>
+                <p>{{ Math.round(statsProduct?.total_stock_value * valyutaKurs) }}</p>
+                <p>UZS</p>
+            </div>
         </div>
-        </div>
-        
+
     </section>
 </template>
