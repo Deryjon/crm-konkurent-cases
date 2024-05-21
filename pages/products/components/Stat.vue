d39dd7fec19684e004be2fcec836dca93a99f595
Dery Jon <yusupoviskandar1005@gmail.com>
2024-05-21 20:51:21 +0500

    edit: edit stats compoennt (2024-05-21, 20:51)

--- pages/products/components/Stat.vue
+++ pages/products/components/Stat.vue
@@ -33,9 +33,9 @@ onMounted(() => {
 
 </script>
 <template>
-    <section class="stats flex flex-wrap justify-between">
+    <section class="stats flex  flex-wrap justify-between">
 
-        <div class="w-[80px] lg:w-[250px] bg-white border-2 dark:bg-[#353535] rounded-2xl p-1 lg:p-3">
+        <div class="w-[150px] lg:w-[250px] bg-white border-2 dark:bg-[#353535] rounded-2xl p-1 lg:p-3">
             <Icon name="heroicons:cube" class="h-5 w-5 text-muted-foreground" />
             <p class="text-sm">Наименований</p>
             <div class="flex items-center gap-1">
@@ -45,7 +45,7 @@ onMounted(() => {
             </div>
         </div>
 
-        <div class="w-[80px] lg:w-[250px] bg-white border-2 dark:bg-[#353535] rounded-2xl p-1 lg:p-3">
+        <div class="w-[150px] lg:w-[250px] bg-white border-2 dark:bg-[#353535] rounded-2xl p-1 lg:p-3">
             <Icon name="heroicons:cube" class="h-5 w-5 text-muted-foreground" />
             <p class="text-sm">Товарных единиц</p>
             <div class="flex items-center gap-1">
@@ -53,15 +53,15 @@ onMounted(() => {
                 <p>ед.</p>
             </div>
         </div>
-        <div class="w-[80px] lg:w-[250px] bg-white border-2 dark:bg-[#353535] rounded-2xl p-1 lg:p-3">
+        <div class="w-[150px] lg:w-[250px] bg-white border-2 dark:bg-[#353535] rounded-2xl p-1 lg:p-3">
             <Icon name="ph:money" class="h-5 w-5 text-muted-foreground" />
             <p class="text-sm">Сумма по цене продажи</p>
             <div class="flex items-center gap-1">
-                <p>{{ statsProduct?.total_stock_value }}</p>
+                <p>{{ ёstatsProduct?.total_stock_value }}</p>
                 <p>USD</p>
             </div>
         </div>
-        <div class="w-[80px] lg:w-[250px] bg-white border-2 dark:bg-[#353535] rounded-2xl p-1 lg:p-3">
+        <div class="w-[150px] lg:w-[250px] bg-white border-2 dark:bg-[#353535] rounded-2xl p-1 lg:p-3">
             <Icon name="ph:money" class="h-5 w-5 text-muted-foreground" />
             <p class="text-sm">Сумма по цене продажи</p>
             <div class="flex items-center gap-1">
