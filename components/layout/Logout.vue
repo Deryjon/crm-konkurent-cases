32a464487f0feaeb51af5f9c71bafaa87529f425
Dery Jon <yusupoviskandar1005@gmail.com>
2024-05-29 11:58:09 +0500

    feat: add bugs

--- components/layout/Logout.vue
+++ components/layout/Logout.vue
@@ -1,9 +1,8 @@
 
 <script setup lang="ts">
-
 </script>
 <template >
-  <div   class="inline-flex items-center gap-4 px-4 p-3 text-left text-[18px] cursor-pointer">
+  <div   class="inline-flex items-center gap-4 px-4 p-3 text-left text-[18px] cursor-pointer text-black dark:text-white ">
           <Icon  name="heroicons:arrow-left-on-rectangle" class="h-5 w-5 text-muted-foreground" />
           <p class="truncate">Выйти</p>
         </div>
--- components/layout/SidebarMobile.vue
+++ components/layout/SidebarMobile.vue
@@ -2,7 +2,7 @@
 import { useRouter } from 'vue-router';
 const router = useRouter();
 
-function logOut(){
+function logOut() {
   localStorage.removeItem('token');
   window.location.reload();
   router.push('/login')
@@ -10,15 +10,11 @@ function logOut(){
 </script>
 <template>
   <aside class="w-[290px] h-[700px] bg-white dark:bg-[#404040]">
-    <div class="flex h-[650px] w-full flex-col gap-3 lg:gap-5 overflow-hidden rounded-md p-[10px]">
+    <div class="flex h-[650px] w-full flex-col gap-3 lg:gap-5 rounded-md p-[10px]">
       <LayoutLogoMobile @click="$router.push('/')" class="  lg:hidden" />
-      <!-- <LayoutLogo @click="$router.push('/')" class="hidden" /> -->
       <LayoutMenu class="" />
-      <div class="mt-auto">
-        <LayoutLogout @click="logOut" />
-        <!-- <LayoutHelp /> -->
-      </div>
+      <LayoutLogout @click="logOut" class="mt-[30px]" />
+
     </div>
   </aside>
 </template>
-
